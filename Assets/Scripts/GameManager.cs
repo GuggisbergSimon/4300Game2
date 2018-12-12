363b0f4815bce01883d86f79393ca2fff56bf011
Unknown <slyguggi@gmail.com>
2018-12-12 15:41:04 +0100

    Added SimonScene, GameManager, NET 4.0

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance = null;
+	public static GameManager Instance => instance;
+
+
+	private void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoadingScene;
+	}
+
+	private void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoadingScene;
+	}
+
+	private void OnLevelFinishedLoadingScene(Scene scene, LoadSceneMode mode)
+	{
+		//Add code to be executed when a new scene is loaded
+	}
+
+	private void CheckEscape()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Quit();
+		}
+	}
+
+	public void LoadLevel(string nameLevel)
+	{
+		SceneManager.LoadScene(nameLevel);
+	}
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+
+}
