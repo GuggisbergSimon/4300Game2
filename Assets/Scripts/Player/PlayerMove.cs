0f1c64981be136b8fa6b9ece420d4891a8233562
Guillaume <j6.guillaume@gmail.com>
2018-12-13 13:41:50 +0100

    Player moves horizontaly

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMove : MonoBehaviour {
 
     [SerializeField]
-    private float playerMaxSpeed = 10.0f;
+    private Rigidbody2D playerRigidbody2D;
     [SerializeField]
-    private float playerAcceleration = 2.0f;
+    private float playerMaxSpeed = 5.0f;
+    [SerializeField]
+    private float playerAcceleration = 50.0f;
 
 
     void Start ()
@@ -20,6 +23,13 @@ public class PlayerMove : MonoBehaviour {
     {
         float horizontal = Input.GetAxis("Horizontal");
 
+        if(Math.Abs(playerRigidbody2D.velocity.x) <= playerMaxSpeed)
+        {
+            Vector3 playerNewVelocity = playerRigidbody2D.velocity;
 
+            playerNewVelocity += new Vector3(playerAcceleration, 0.0f, 0.0f) * horizontal * Time.deltaTime;
+
+            playerRigidbody2D.velocity = playerNewVelocity;
+        }
 	}
 }
