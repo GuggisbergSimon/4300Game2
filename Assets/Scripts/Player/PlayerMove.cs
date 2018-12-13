bb5f10867204bd1df73f59c4d9a4255ee8210ca2
Guillaume <j6.guillaume@gmail.com>
2018-12-13 13:20:55 +0100

    playerMove begining

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField]
+    private float playerMaxSpeed = 10.0f;
+    [SerializeField]
+    private float playerAcceleration = 2.0f;
+
+
+    void Start ()
+    {
+		
+
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+	}
+}
